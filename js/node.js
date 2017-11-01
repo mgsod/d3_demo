e921b6f04e29299e41bbffba0ae4514e439ce7ba
setting <setting@transing.cn>
2017-11-01 16:33:42 +0800

    s

--- js/node.js
+++ js/node.js
@@ -8,6 +8,7 @@ var Node = {
      * @param options
      */
     init: function (options) {
+        this.vue_setting = options.vue_setting;
         this.canvas = options.canvas; //画布
         this.nodeSetting = options.nodeSetting; //节点属性
         this.nodeList = options.nodeList; //节点列表 公用属性
@@ -29,18 +30,34 @@ var Node = {
         this.onCreateNode = options.onCreateNode
 
 
-        window.onload = setSvgSize;
-        window.onresize = setSvgSize;
+        window.onload = setSvgSize(this);
+        window.onresize = setSvgSize(this);
 
-        function setSvgSize() {
-            Node.svgWidth = $('.canvas').width();
-            Node.svgHeight = $(window).height() - 60;
+        function setSvgSize(_this) {
+
+            _this.svgWidth = $('.canvas').width();
+            _this.svgHeight = $(window).height() - 60;
             $('.bgContainer,svg').css(
                 {
-                    'width': Node.svgWidth,
-                    'height': Node.svgHeight
+                    'width': _this.svgWidth,
+                    'height': _this.svgHeight
                 })
         }
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "progressBar": false,
+            "positionClass": "toast-top-center",
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "3000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "swing",
+            "showMethod": "fadeIn",
+            "hideMethod": "slideUp"
+        };
 
         this.canvas.append('svg:defs').append('svg:marker')
             .attr('id', 'end-arrow')
@@ -60,8 +77,9 @@ var Node = {
      */
     createNode: function () {
         var _this = this;
-        var g = Node.canvas.selectAll('g')
-            .data(Node.nodeList)
+
+        var g = _this.canvas.selectAll('g')
+            .data(_this.nodeList)
             .enter()
             .append('g')
             .attr('transform', function (d) {
@@ -76,7 +94,7 @@ var Node = {
                 if (!name) {
                     //如果是新建则取时间戳作为唯一名
                     var time = new Date().getTime();
-                    d.nodeInfo.name = 'node_1' + time;
+                    d.nodeInfo.name = 'node_' + time;
                     return 'node_' + time;
                 } else {
                     //如果更新节点则以之前名为命名
@@ -88,22 +106,22 @@ var Node = {
 
         //画矩形
         g.append('rect')
-            .attr('width', Node.nodeWidth)
-            .attr('height', Node.nodeHeight)
+            .attr('width', _this.nodeWidth)
+            .attr('height', _this.nodeHeight)
             .style({
                 fill: 'none'
             });
         //图片
         g.append('image')
-            .attr('width', Node.nodeWidth)
-            .attr('height', Node.nodeHeight)
+            .attr('width', _this.nodeWidth)
+            .attr('height', _this.nodeHeight)
             .attr('xlink:href', function (d) {
-                return Node.nodeSetting[d.nodeInfo.type].img
+                return _this.nodeSetting[d.nodeInfo.type].img
             });
         //绑定单击事件
         g.on('click', function (d) {
             if (d3.event.defaultPrevented) return; //防止拖动触发单击事件
-            Node.clickNode(g, d);
+            _this.clickNode(g, d);
 
 
         });
@@ -112,17 +130,17 @@ var Node = {
             .attr('points', '53,15 53,35 65,25')
             .attr('fill', '#fff')
             .attr('stroke', function (d) {
-                return Node.nodeSetting[d.nodeInfo.type].color
+                return _this.nodeSetting[d.nodeInfo.type].color
             })
             .attr('stroke-width', 1)
             .classed('polygon show', true)
             .on('click', function (d) {
                 d3.select(this)
                     .attr('fill', function (d) {
-                        return Node.nodeSetting[d.nodeInfo.type].color
+                        return _this.nodeSetting[d.nodeInfo.type].color
                     });
                 d3.event.stopPropagation();
-                Node.drawLine(g, d);
+                _this.drawLine(g, d);
             });
         /*g.append('circle')
             .attr('cx',55)
@@ -143,8 +161,8 @@ var Node = {
         });*/
 
         //绑定拖拽事件
-        Node.canvas.selectAll('g')
-            .call(Node.drag());
+        _this.canvas.selectAll('g')
+            .call(_this.drag(_this));
 
 
     },
@@ -152,7 +170,7 @@ var Node = {
     /**
      * 定义d3拖拽.包括设置拖拽时的圆心位置
      */
-    drag: function () {
+    drag: function (_this) {
         return d3.behavior.drag()
             .origin(function () {
                 //以鼠标点击的位置作为拖动的圆心.防止开始拖动时抖动
@@ -164,7 +182,7 @@ var Node = {
                     x: x_y[1],
                     y: x_y[2]
                 }
-            }).on("drag", Node.dragMove); //绑定拖拽函数
+            }).on("drag", _this.dragMove(_this)); //绑定拖拽函数
 
     },
 
@@ -174,49 +192,51 @@ var Node = {
      * @param _nodeIndex
      * @returns {boolean}
      */
-    dragMove: function (_nodeData, _nodeIndex) {
-        //将进行边缘碰撞计算后的值赋值给节点对象的x,y.
-        _nodeData.nodeInfo.x = Node.computedPosition('x', d3.event.x);
-        _nodeData.nodeInfo.y = Node.computedPosition('y', d3.event.y);
+    dragMove: function (_this) {
+        return function(_nodeData,_nodeIndex){
+            //将进行边缘碰撞计算后的值赋值给节点对象的x,y.
+            _nodeData.nodeInfo.x = _this.computedPosition('x', d3.event.x);
+            _nodeData.nodeInfo.y = _this.computedPosition('y', d3.event.y);
+            //除开本节点 其他需要进行碰撞检测的节点
+            var collisionArr = _this.copyArr(_this.nodeList, _nodeIndex);
+            //获取其他节点离本节点最近的节点索引
+            var minDistances = _this.getMinDistanceIndex(_nodeData.nodeInfo.x, _nodeData.nodeInfo.y, collisionArr);
 
-        //除开本节点 其他需要进行碰撞检测的节点
-        var collisionArr = Node.copyArr(nodeList, _nodeIndex);
-        //获取其他节点离本节点最近的节点索引
-        var minDistances = Node.getMinDistanceIndex(_nodeData.nodeInfo.x, _nodeData.nodeInfo.y, collisionArr);
 
+            //判断节点拖动时. 是否需要线条跟随
+            if (_this.allowPath) {
+                var name = _nodeData.nodeInfo.name;
+                var toPath = _this.canvas.selectAll('[to=' + name + ']');
+                var fromPath = _this.canvas.selectAll('[from=' + name + ']');
+                if (toPath.size() > 0) {
+                    toPath.filter(function () {
+                        var toD = d3.select(this).attr('d');
+                        //正则获取并替换线条的终点               //加上偏移量 以让箭头指向圆心
+                        toD = toD.replace(/L(\d+) (\d)+/, 'L' + (_nodeData.nodeInfo.x + _this.nodeOffset) + ' ' + (_nodeData.nodeInfo.y + _this.nodeOffset));
+                        d3.select(this).attr('d', toD);
+                    });
+                }
 
-        //判断节点拖动时. 是否需要线条跟随
-        if (Node.allowPath) {
-            var name = _nodeData.nodeInfo.name;
-            var toPath = Node.canvas.selectAll('[to=' + name + ']');
-            var fromPath = Node.canvas.selectAll('[from=' + name + ']');
-            if (toPath.size() > 0) {
-                toPath.filter(function () {
-                    var toD = d3.select(this).attr('d');
-                    //正则获取并替换线条的终点               //加上偏移量 以让箭头指向圆心
-                    toD = toD.replace(/L(\d+) (\d)+/, 'L' + (_nodeData.nodeInfo.x + Node.nodeOffset) + ' ' + (_nodeData.nodeInfo.y + Node.nodeOffset));
-                    d3.select(this).attr('d', toD);
-                });
+                if (fromPath.size() > 0) {
+                    fromPath.filter(function () {
+                        var fromD = d3.select(this).attr('d');
+                        //正则获取并替换线条的起点                   //加上偏移量 以让箭头指向圆心
+                        fromD = fromD.replace(/M(\d+) (\d)+/, 'M' + (_nodeData.nodeInfo.x + _this.nodeOffset) + ' ' + (_nodeData.nodeInfo.y + _this.nodeOffset));
+                        d3.select(this).attr('d', fromD)
+                    })
+                }
             }
-
-            if (fromPath.size() > 0) {
-                fromPath.filter(function () {
-                    var fromD = d3.select(this).attr('d');
-                    //正则获取并替换线条的起点                   //加上偏移量 以让箭头指向圆心
-                    fromD = fromD.replace(/M(\d+) (\d)+/, 'M' + (_nodeData.nodeInfo.x + Node.nodeOffset) + ' ' + (_nodeData.nodeInfo.y + Node.nodeOffset));
-                    d3.select(this).attr('d', fromD)
-                })
+            //判断是否有节点可以碰撞. 如果有则拖动结束
+            if (minDistances > -1 && _this.isCollisionWithRect(_nodeData.nodeInfo.x, _nodeData.nodeInfo.y, _this.nodeWidth, _this.nodeHeight, collisionArr[minDistances].nodeInfo.x, collisionArr[minDistances].nodeInfo.y)) {
+                _this.allowPath = false;
+                return false;
+            } else {
+                _this.allowPath = true;
             }
+            d3.select(this)
+                .attr('transform', 'translate(' + _nodeData.nodeInfo.x + ',' + _nodeData.nodeInfo.y + ')')
         }
-        //判断是否有节点可以碰撞. 如果有则拖动结束
-        if (minDistances > -1 && Node.isCollisionWithRect(_nodeData.nodeInfo.x, _nodeData.nodeInfo.y, Node.nodeWidth, Node.nodeHeight, collisionArr[minDistances].nodeInfo.x, collisionArr[minDistances].nodeInfo.y)) {
-            Node.allowPath = false;
-            return false;
-        } else {
-            Node.allowPath = true;
-        }
-        d3.select(this)
-            .attr('transform', 'translate(' + _nodeData.nodeInfo.x + ',' + _nodeData.nodeInfo.y + ')')
+
     },
 
     /**
@@ -225,13 +245,14 @@ var Node = {
      * @param _nodeData 节点数据
      */
     clickNode: function (_node, _nodeData) {
+        var _this = this;
         var type = _nodeData.nodeInfo.type;
         var data = _nodeData.data;
         var name = _nodeData.nodeInfo.name;
-        v_nodeSetting.type = type;
-        v_nodeSetting.data[type] = data;
-        v_nodeSetting.name = name;
-        Node.canvas.selectAll('g')
+        _this.vue_setting.type = type;
+        _this.vue_setting.data[type] = data;
+        _this.vue_setting.name = name;
+        _this.canvas.selectAll('g')
             .filter(function (data) {
                 if (data.nodeInfo.name === _nodeData.nodeInfo.name) {
                     var _this = d3.select(this);
@@ -265,55 +286,55 @@ var Node = {
      * @returns {boolean}
      */
     drawLine: function (_node, _nodeData) {
-        if (!Node.isSelectStart) {
-            Node.selectedNodeData = _nodeData;
-            Node.selectedNode = _node;
-            Node.isSelectStart = true;
+        var _this = this;
+        if (!_this.isSelectStart) {
+            _this.selectedNodeData = _nodeData;
+            _this.selectedNode = _node;
+            _this.isSelectStart = true;
         } else {
-            if (_node === Node.selectedNode) {
+            if (_node === _this.selectedNode) {
                 toastr['warning']("不能选择当前节作为下级节点");
-                Node.restLine(_node);
+                _this.restLine(_node);
                 return false
             }
-            if (_nodeData.nodeInfo.from && _nodeData.nodeInfo.from.indexOf(Node.selectedNodeData.nodeInfo.name) > -1) {
+            if (_nodeData.nodeInfo.from && _nodeData.nodeInfo.from.indexOf(_this.selectedNodeData.nodeInfo.name) > -1) {
                 toastr['warning']("此节点已经是当前节点的下级");
-                Node.restLine(_node);
+                _this.restLine(_node);
                 return false;
             }
-            if (_nodeData.nodeInfo.to && _nodeData.nodeInfo.to.indexOf(Node.selectedNodeData.nodeInfo.name) > -1) {
+            if (_nodeData.nodeInfo.to && _nodeData.nodeInfo.to.indexOf(_this.selectedNodeData.nodeInfo.name) > -1) {
                 toastr['warning']("此节点是当前节点的上级,不可作为下级节点");
-                Node.restLine(_node);
+                _this.restLine(_node);
                 return false;
             }
-            Node.canvas.append('path')
+            _this.canvas.append('path')
                 .attr('d', function () {
-                    return 'M' + (Node.selectedNodeData.nodeInfo.x + Node.nodeOffset) + ' ' + (Node.selectedNodeData.nodeInfo.y + Node.nodeOffset) + ' L' + (_nodeData.nodeInfo.x + Node.nodeOffset) + ' ' + (_nodeData.nodeInfo.y + Node.nodeOffset)
+                    return 'M' + (_this.selectedNodeData.nodeInfo.x + _this.nodeOffset) + ' ' + (_this.selectedNodeData.nodeInfo.y + _this.nodeOffset) + ' L' + (_nodeData.nodeInfo.x + _this.nodeOffset) + ' ' + (_nodeData.nodeInfo.y + _this.nodeOffset)
                 })
                 .attr("marker-end", "url(#end-arrow)")
-                .attr('from', Node.selectedNodeData.nodeInfo.name)
+                .attr('from', _this.selectedNodeData.nodeInfo.name)
                 .attr('to', _nodeData.nodeInfo.name)
                 .attr('class', 'line')
                 .style({
                     fill: 'none',
-                    stroke: Node.pathColor,
+                    stroke: _this.pathColor,
                     'stroke-width': 1.5
                 })
                 .on('mouseover', function () {
                     d3.select(this).style({stroke: '#ffad33', 'stroke-width': 2.8})
                 })
                 .on('mouseout', function () {
-                    d3.select(this).style({stroke: Node.pathColor, 'stroke-width': 1.5})
+                    d3.select(this).style({stroke: _this.pathColor, 'stroke-width': 1.5})
                 });
-
-            Node.selectedNodeData.nodeInfo.to = Node.selectedNodeData.nodeInfo.to || [];
-            Node.selectedNodeData.nodeInfo.to.push(_nodeData.nodeInfo.name);
+            _this.selectedNodeData.nodeInfo.to = _this.selectedNodeData.nodeInfo.to || [];
+            _this.selectedNodeData.nodeInfo.to.push(_nodeData.nodeInfo.name);
 
             _nodeData.nodeInfo.from = _nodeData.nodeInfo.from || [];
-            _nodeData.nodeInfo.from.push(Node.selectedNodeData.nodeInfo.name);
+            _nodeData.nodeInfo.from.push(_this.selectedNodeData.nodeInfo.name);
 
             this.onDrawLine && this.onDrawLine();
 
-            Node.restLine(_node)
+            _this.restLine(_node)
         }
     },
 
@@ -326,21 +347,22 @@ var Node = {
      * 如果未传入则视为仅仅删除线条 移除起点和终点中对应的项
      */
     delPath: function (path, tag) {
+        var _this = this;
         var path_to = path.attr('to');
         var path_from = path.attr('from');
 
 
         if (!tag || tag === "from") {
             //移除终点节点中from的项
-            var index_to = Node.getNodeIndexByName(nodeList, path_to);
-            var toNode = nodeList[index_to];
+            var index_to = _this.getNodeIndexByName(_this.nodeList, path_to);
+            var toNode = _this.nodeList[index_to];
             index_to && toNode.nodeInfo.from.splice(toNode.nodeInfo.from.indexOf(path_from), 1);
         }
 
         if (!tag || tag === "to") {
             //移除起点节点中to的项
-            var index_from = Node.getNodeIndexByName(nodeList, path_from);
-            var fromNode = nodeList[index_from];
+            var index_from = _this.getNodeIndexByName(_this.nodeList, path_from);
+            var fromNode = _this.nodeList[index_from];
             index_from && fromNode.nodeInfo.to.splice(fromNode.nodeInfo.to.indexOf(path_to), 1);
         }
 
@@ -353,24 +375,26 @@ var Node = {
      * @param node 选中的节点对象[d3]
      */
     delNode: function (node) {
+
+        var _this = this;
         var name = node.attr('id');
         node.remove();
-        Node.nodeList.splice(Node.getNodeIndexByName(Node.nodeList, name), 1);
+        _this.nodeList.splice(_this.getNodeIndexByName(_this.nodeList, name), 1);
         d3.selectAll('[from=' + name + ']')
             .filter(function () {
-                Node.delPath(d3.select(this), 'from');
+                _this.delPath(d3.select(this), 'from');
 
             });
         d3.selectAll('[to=' + name + ']')
             .filter(function () {
-                Node.delPath(d3.select(this), 'to');
+                _this.delPath(d3.select(this), 'to');
             });
 
 
         //绑定拖拽事件
-        Node.canvas.selectAll('g')
-            .call(Node.drag());
-        Node.selectedNodeData = null;
+        _this.canvas.selectAll('g')
+            .call(_this.drag(_this));
+        _this.selectedNodeData = null;
     },
 
     /**
@@ -378,14 +402,15 @@ var Node = {
      * @param _node 当前选中的节点
      */
     restLine: function (_node) {
+        var _this = this;
         setTimeout(function () {
-            Node.selectedNode.select('polygon')
+            _this.selectedNode.select('polygon')
                 .attr('fill', '#fff');
             _node.select('polygon')
                 .attr('fill', '#fff')
         }, 1100);
-        Node.selectedNodeData = null;
-        Node.isSelectStart = false;
+        _this.selectedNodeData = null;
+        _this.isSelectStart = false;
     },
 
     /**
@@ -395,23 +420,24 @@ var Node = {
      * @returns {temp} 返回的合适数值
      */
     computedPosition: function (x_y, value) {
+        var _this = this;
         var temp = value;
         switch (x_y) {
             case 'x':
-                if (value <= 0 || value <= Node.adsorptionIntensity) {
+                if (value <= 0 || value <= _this.adsorptionIntensity) {
                     temp = 0;
                 }
-                if (value >= Node.svgWidth - Node.nodeWidth || value >= Node.svgWidth - Node.nodeWidth - Node.adsorptionIntensity) {
-                    temp = Node.svgWidth - Node.nodeWidth;
+                if (value >= _this.svgWidth - _this.nodeWidth || value >= _this.svgWidth - _this.nodeWidth - _this.adsorptionIntensity) {
+                    temp = _this.svgWidth - _this.nodeWidth;
                 }
                 return parseInt(temp);
                 break;
             case 'y':
-                if (value <= 0 || value <= Node.adsorptionIntensity) {
+                if (value <= 0 || value <= _this.adsorptionIntensity) {
                     temp = 0;
                 }
-                if (value >= Node.svgHeight - Node.nodeHeight || value >= Node.svgHeight - Node.nodeHeight - Node.adsorptionIntensity) {
-                    temp = Node.svgHeight - Node.nodeHeight;
+                if (value >= _this.svgHeight - _this.nodeHeight || value >= _this.svgHeight - _this.nodeHeight - _this.adsorptionIntensity) {
+                    temp = _this.svgHeight - _this.nodeHeight;
                 }
                 return parseInt(temp);
                 break;
@@ -440,9 +466,10 @@ var Node = {
      * @returns {number}
      */
     getMinDistanceIndex: function (x, y, arr) {
+        var _this = this;
         var distances = [];
         for (var i = 0; i < arr.length; i++) {
-            distances.push(Node.getDistance(x, y, arr[i].nodeInfo.x, arr[i].nodeInfo.y))
+            distances.push(_this.getDistance(x, y, arr[i].nodeInfo.x, arr[i].nodeInfo.y))
         }
         return distances.indexOf(Math.min.apply(Math, distances))
     },
